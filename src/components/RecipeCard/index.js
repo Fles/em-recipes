33e8e64a4c94c051b03106fa6c7608bfa06b848d
Fles <nikolahr@gmail.com>
2017-04-21 21:25:44 +0200

    Add flip efect to show the whole recipe

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -16,31 +16,77 @@ class RecipeCard extends Component {
     let { cook_time } = data;
 
     let color = colors.find(c => cook_time >= c.min && cook_time <= c.max);
-
+    let cId = 'C' + data.id;
     return (
-      <Card
-        className={classnames(classname, styles.RecipeCard)}
-        image='http://lorempixel.com/600/600/food/'
-        header={
-          <div>
-            <Rating icon='heart' onRate={() => {checkRecipe(data)}}/>
-            <h3>{ data.name }</h3>
+        <div id={cId} className={classnames(styles.RecipeCard, "flip-container")}>
+          <div className="flipper">
+            <div className="front">
+              <Card
+                onClick={()=>{checkRecipe(data)}}
+                className={classnames(classname)}
+                image='http://lorempixel.com/600/600/food/'
+                header={
+                  <div>
+                    <Rating
+                      icon='heart'
+                      onRate={() => {checkRecipe(data)}}
+                     />
+                    <h3>{ data.name }</h3>
+                  </div>
+                }
+                meta={ data.type }
+                description={
+                  <span className="ingredients">
+                    { data.ingredients.join(', ') }
+                  </span>
+                }
+                extra={
+                  <span>
+                    <Icon name='hourglass start' />
+                    <strong>{ cook_time }</strong> min
+                    <br/>
+                    <Icon
+                      name='retweet'
+                      onClick={()=>{
+                        document.querySelector("#"+cId).classList.toggle("flip")
+                      }}
+                    />
+                  </span>
+                }
+                color={color.name}
+              />
+            </div>
+            <div className="back">
+              <Card
+                  onClick={()=>{}}
+                  className={classnames(classname, styles.RecipeCard)}
+                  image=''
+                  header={
+                    <div>
+                      <h3>{ data.name }</h3>
+                    </div>
+                  }
+                  meta={ data.type }
+                  description={
+                    <span className="ingredients">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Coas mollitia necessitatibus nostrum quasi, ratione voluptas. Ab ad aliquid assumenda corporis cupiditate, eaque eligendi error iure labore omnis placeat porro quibusdam quos reiciendis repellat repudiandae tempora voluptas voluptatem? Ad cupiditate deserunt ducimus eaque fuga harum nihil nulla officia perspiciatis, ratione? A accusantium amet assumenda, commodi corporis culpa cumque enim eveniet illo impedit iste iusto magni natus nobis nostrum nulla odit pariatur perferendis porro praesentium provident, similique, sunt suscipit! Accusamus amet, assumenda at blanditiis corporis culpa deserunt distinctio dolor dolore dolorum eius et, ex explicabo illo inventore ipsa laboriosam minima nam natus provident quam quia reprehenderit sed sequi sint sit voluptates! Aliquid aspernatur autem deserunt expedita impedit, odio perspiciatis quo suscipit. Aperiam aspernatur deserunt, dolorum eos est facere hic libero, nam officia officiis quaerat quibusdam!
+                    </span>
+                }
+                  extra={
+                  <span>
+                    <Icon
+                      name='retweet'
+                      onClick={()=>{
+                        document.querySelector("#"+cId).classList.toggle("flip")
+                      }}
+                    />
+                  </span>
+                }
+                  color={color.name}
+              />
+            </div>
           </div>
-        }
-        meta={ data.type }
-        description={
-          <span className="ingredients">
-            { data.ingredients.join(', ') }
-          </span>
-        }
-        extra={
-          <span>
-            <Icon name='hourglass start' />
-            <strong>{ cook_time }</strong> min
-          </span>
-        }
-        color={color.name}
-      />
+        </div>
     )
   }
 }
--- src/reducer.js
+++ src/reducer.js
@@ -5,10 +5,11 @@ const initialState = Immutable.fromJS({
   recipes: data.map((r, i) => ({...r, id: i, checked: false}))
 });
 
-export default function recipes(state = initialState, action) {
+export default function reducer(state = initialState, action) {
   switch (action.type) {
     case types.CHECK_RECIPE:
-      return state.setIn(['recipes', action.payload.id, 'name'], 'kujac');
+      let ap = action.payload;
+      return state.setIn(['recipes', ap.id, 'checked'], !ap.checked);
     default:
       return state;
   }
